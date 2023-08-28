35d8c959c037259e188b9529e9baaf19b167bf94
a.kurkin <a.kurkin@qiwi.com>
2023-08-28 23:45:42 +0300

    Урок 18 (Домашнее задание)

---
+++ src/main/java/ru/otus/askurkin/AppLesson18.java
@@ -0,0 +1,66 @@
+package ru.otus.askurkin;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AppLesson18 {
+
+//	Описание/Пошаговая инструкция выполнения домашнего задания:
+//Реализуйте приложение, позволяющее работать с текстовыми файлами
+//При старте приложения, в консоль выводится список текстовых файлов из корневого каталоге проекта
+//Далее программа запрашивает имя файла, с которым хочет работать пользователь
+//Содержимое файла выводится в консоль
+//Затем любую введенную пользователем строку необходимо записывать в указанный файл
+
+	public static void main(String[] args) {
+		File dir = new File(".");
+		for (File file : dir.listFiles()) {
+			if (file.isFile() & file.getName().endsWith(".txt")) {
+				System.out.println(file.getName());
+			}
+		}
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Введите имя файла: ");
+		String fileName = scanner.next();
+
+		try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(fileName))) {
+			int ch = in.read();
+			while (ch != -1) {
+				System.out.print((char) ch);
+				ch = in.read();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Файл " + fileName + " не найден.");
+			return;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		System.out.println("\nВведите текст: ");
+		String addString = scanner.next();
+
+		try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(fileName, true))) {
+			byte[] buff = ("\n" + addString).getBytes(StandardCharsets.UTF_8);
+
+			out.write(buff, 0, buff.length);
+
+			out.flush();
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		System.out.println("Добавили в файл.");
+	}
+}
