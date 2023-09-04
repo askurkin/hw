c20ec9afad6255bcd98d971d1e27a911b68090b7
a.kurkin <a.kurkin@qiwi.com>
2023-09-04 18:47:01 +0300

    Урок 17 (Домашнее задание)

--- src/main/java/ru/otus/askurkin/lesn17/SearchTree.java
+++ src/main/java/ru/otus/askurkin/lesn17/SearchTree.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 public interface SearchTree<T> {
 
-	// @param element to find
-	// @return element if exists, otherwise - null
+	/**
+	 * @param element to find
+	 * @return element if exists, otherwise - null
+	 */
 
 	T find(T element);
 
